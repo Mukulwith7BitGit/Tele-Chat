7db685828444d1197441d93ecefa2169093debea
Mukul Dalal <mukuldalalnfs@gmail.com>
2024-05-13 15:17:16 +0530

    code fixes

--- backend/index.js
+++ backend/index.js
@@ -25,10 +25,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const routes=require('./routes/routes');
-const {Server} = require('socket.io');
+const routes = require('./routes/routes');
+const { Server } = require('socket.io');
 
-const http=require('http');
+const http = require('http');
 const app = express();
 // const httpServer=http.createServer(app);
 // const io=new Server(httpServer);
@@ -37,11 +37,11 @@ const io = require('socket.io')(httpServer, {
     cors: { origin: "*" },
     // credentials:true,
 });
-require('dotenv').config(); 
+require('dotenv').config();
 // io.origins('*:*');
-let uRoomNo="";
-const corsOptions={
-    credentials:true,
+let uRoomNo = "";
+const corsOptions = {
+    credentials: true,
     origin: 'https://tele-chat-77dg.onrender.com',
     // credentials:true,
     // methods: ["GET", "POST"],
@@ -56,25 +56,25 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log('A user connected');
 
-    socket.on('joinRoom',(message)=>{
-        let uRoomNo=message;
-        console.log("received room no. on backend is: "+uRoomNo);
-    
+    socket.on('joinRoom', (message) => {
+        let uRoomNo = message;
+        console.log("received room no. on backend is: " + uRoomNo);
+
         // const socket=io.sockets.sockets[socket.id];
-        socket.room="room-"+uRoomNo;
-        socket.join("room-"+uRoomNo);
+        socket.room = "room-" + uRoomNo;
+        socket.join("room-" + uRoomNo);
     });
 
     socket.on('sendMsg', (message) => {
-        console.log("sendMsg event is going to be called: "+message.text);
+        console.log("sendMsg event is going to be called: " + message.text);
 
         // io.to("room-"+uRoomNo).
-        io.emit('sendMsgEvent',message.text);
+        io.emit('sendMsgEvent', message.text);
     });
-    socket.on('disconnect',()=>{
+    socket.on('disconnect', () => {
         console.log('A user disconnected');
     });
 });
@@ -82,21 +82,21 @@ io.on('connection',(socket)=>{
 
 
 //routes
-app.use('/api',routes);
+app.use('/api', routes);
 const PORT = process.env.PORT || 5000;
 const mongodb_uri = process.env.MONGODB_CONNECTION_STRING;
 
 // mongoose.connect("mongodb://172.17.0.2:27017/telechat",{
 // mongoose.connect("mongodb://127.0.0.1:27017/telechat",{
-mongoose.connect(mongodb_uri,{
-    useNewUrlParser:true
-}).then(()=>{
+mongoose.connect(mongodb_uri, {
+    useNewUrlParser: true
+}).then(() => {
     console.log("connected to database");
-    httpServer.listen(PORT,()=>{
+    httpServer.listen(PORT, () => {
         console.log('Server is listening on port 5000');
     });
-}).catch((error)=>{
-    console.error('Error connecting to the database: ',error);
+}).catch((error) => {
+    console.error('Error connecting to the database: ', error);
 });
 
 
@@ -137,5 +137,5 @@ mongoose.connect(mongodb_uri,{
 
 
 // mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb
-// // 
+// //
 // mongoose.connect("mongodb://localhost:27017/telechat",{
